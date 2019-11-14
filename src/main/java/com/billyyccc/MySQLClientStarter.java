384bd8cb41bd915b15fe2cc82ad7ce5a0765a02e
Billy Yuan <billy112487983@gmail.com>
2019-11-14 20:35:08 +0800

    more tests

--- src/main/java/com/billyyccc/MySQLClientStarter.java
+++ src/main/java/com/billyyccc/MySQLClientStarter.java
@@ -6,9 +6,11 @@ import io.vertx.sqlclient.PoolOptions;
 import io.vertx.sqlclient.SqlConnection;
 import io.vertx.sqlclient.Tuple;
 
+import java.sql.*;
+
 public class MySQLClientStarter {
     public static void main(String[] args) {
-        MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+      MySQLConnectOptions connectOptions = new MySQLConnectOptions()
                 .setHost("localhost")
                 .setPort(3306)
                 .setUser("root")
@@ -28,6 +30,29 @@ public class MySQLClientStarter {
             conn.preparedQuery("INSERT INTO news (Title, Summery, Story) VALUES (?, ?, ?)",tuple, ar -> {
                 if (ar.succeeded()) {
                     System.out.println("SUCCESS");
+                    // jdbc retrieving after inserting with reactive-mysql-client
+                  try {
+                    Connection jdbcConn =
+                      DriverManager.getConnection("jdbc:mysql://localhost/mysql?" +
+                        "user=root&password=password&useSSL=false&useServerPrepStmts=TRUE&useUnicode=true&characterEncoding=utf-8");
+
+                    String sql = "SELECT * FROM news WHERE story = 'this is a story';";
+                    ResultSet resultSet = jdbcConn.createStatement().executeQuery(sql);
+                    while (resultSet.next()) {
+                      String title = resultSet.getString("Title");
+                      String summery = resultSet.getString("Summery");
+                      System.out.println("title: [" + title + "]");
+                      System.out.println("summery: [" + summery + "]");
+                      System.out.println(title.equals("اعلن مطوري فيجول نوفل World End Economica"));
+                      System.out.println(summery.equals("الإعلان عن مشروع World End Economica"));
+                    }
+
+                  } catch (SQLException ex) {
+                    // handle any errors
+                    System.out.println("SQLException: " + ex.getMessage());
+                    System.out.println("SQLState: " + ex.getSQLState());
+                    System.out.println("VendorError: " + ex.getErrorCode());
+                  }
                 } else {
                     ar.cause().printStackTrace();
                 }
